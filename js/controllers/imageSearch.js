b5deb2b038a2c57de1454a23a8e3029d2187b4f2
Martha Fairbanks <marthafairbanks@gmail.com>
2016-07-28 10:56:41 -0400

    fixing merge conflict

--- js/controllers/imageSearch.js
+++ js/controllers/imageSearch.js
@@ -4,6 +4,7 @@
 angular
     .module('flowers')
     .controller('mainController', function(API, $location) {
+    	var vm = this;
 
             vm.search = function() {
                 $location.path('/results/'+vm.searchImage);
--- js/main.js
+++ js/main.js
@@ -1,63 +1,60 @@
 (function() {
-    'use strict';
-    angular
-        .module('flowers', ['ui.router', 'backand'])
+   'use strict';
+   angular
+       .module('flowers', ['ui.router', 'backand'])
 
-    .config(function($stateProvider, $urlRouterProvider, BackandProvider) {
+   .config(function($stateProvider, $urlRouterProvider, BackandProvider) {
 
-        $urlRouterProvider.otherwise('/');
+       $urlRouterProvider.otherwise('/');
 
-        $stateProvider
-            .state('home', {
-                url: '/',
-                views: {
-                    'main': {
-                        templateUrl: '../views/home.html',
-                        controller: 'textController',
-                        controllerAs: 'controller'
-                    }
-                },
-            })
-            .state('text', {
-                url: '/text',
-                views: {
-                    'main': {
-                        templateUrl: '../views/text.html',
-                        controller: 'textController',
-                        controllerAs: 'controller'
-                    }
-                },
-            })
-            .state('imageSearch', {
-                url: '/imageSearch',
-                views: {
-                    'main': {
-                        templateUrl: '../views/partials/imageSearch.html',
-                        controller: 'mainController',
-                        controllerAs: 'controller'
-                    }
-                },
-            })           
-            .state('contact', {
-                url: '/contact',
-                templateUrl: '/views/partials/contact.html',
-                controller: 'contactController',
-                controllerAs: 'controller'
-            })
-            .state('results', {
-                url: '/results/:country',
-                templateUrl: '/views/partials/results.html',
-                controller: 'resultsController',
-                controllerAs: 'controller'
-                views: {
-                    'main': {
-                        templateUrl: '/views/partials/results.html',
-                        controller: 'resultsController',
-                        controllerAs: 'controller'
-                    }
-                },
-            });
+       $stateProvider
+           .state('home', {
+               url: '/',
+               views: {
+                   'main': {
+                       templateUrl: '../views/home.html',
+                       controller: 'textController',
+                       controllerAs: 'controller'
+                   }
+               },
+           })
+           .state('text', {
+               url: '/text',
+               views: {
+                   'main': {
+                       templateUrl: '../views/text.html',
+                       controller: 'textController',
+                       controllerAs: 'controller'
+                   }
+               },
+           })
+           .state('imageSearch', {
+               url: '/imageSearch',
+               views: {
+                   'main': {
+                       templateUrl: '../views/partials/imageSearch.html',
+                       controller: 'mainController',
+                       controllerAs: 'controller'
+                   }
+               },
+           })           
+           .state('contact', {
+               url: '/contact',
+               templateUrl: '/views/partials/contact.html',
+               controller: 'contactController',
+               controllerAs: 'controller'
+           })
+           .state('results', {
+               url: '/results/:country',
+               views: {
+                   'main': {
+                       templateUrl: '/views/partials/results.html',
+                       controller: 'resultsController',
+                       controllerAs: 'controller'
+                   }
+               },
+           });
 
-    });
+   });
 })();
 
