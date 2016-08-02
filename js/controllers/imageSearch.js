5af29c974fb45ebacfdc57a4c73af2e0b78c20c4
Adam Jones <adamjns802@gmail.com>
2016-08-02 12:35:54 -0400

    multiple image search sizes

--- js/controllers/imageSearch.js
+++ js/controllers/imageSearch.js
@@ -7,19 +7,28 @@
             var vm = this;
 
             vm.search = function() {
-                /*$location.path('/results/'+vm.searchImage);*/
 
                 var search = vm.getInfo;
 
-                var data = API.getInfo(search);
+                var data = API.getInfo(search,200);
                 data.then(function(response) {
-                    vm.data = response.data.value;
+                    vm.data = response.data.value[0];
                 });
 
-                //var data1 = API.getInfo(search);
-                //data1.then(function(response) {
-                    //vm.data1 = response.data.value;
-                //});
+                var data1 = API.getInfo(search,350);
+                data1.then(function(response) {
+                    vm.data1 = response.data.value[0];
+                });
+
+                var data2 = API.getInfo(search,500);
+                data2.then(function(response) {
+                    vm.data2 = response.data.value[0];
+                });
+
+                var data3 = API.getInfo(search,750);
+                data3.then(function(response) {
+                    vm.data3 = response.data.value[0];
+                });
             };
 
         });
--- js/factories/api.js
+++ js/factories/api.js
@@ -4,35 +4,34 @@
         .module('flowers')
         .factory('API', function($http) {
 
-            var getInfo = function(image) {
+            var getInfo = function(image,size) {
 
                 var call = $http({
                     headers: {
                         "Ocp-Apim-Subscription-Key": "da6b094ff8914d0597d301ebb935eefd",
                     },
                     method: 'GET',
-                    url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + image + "&count=4&height=250&width=250&aspect=square",
+                    url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + image + "&height="+size+"&width="+size,
                 });
 
                 return call;
-            }
-            var saveUserId = function(userid)
-            {
-                localStorage.setItem('userID',userid);
+
             }
 
-            var getUserId = function()
-            {
+            var saveUserId = function(userid) {
+                localStorage.setItem('userID', userid);
+            }
+
+            var getUserId = function() {
                 return localStorage.getItem('userID');
             }
-            var logout = function()
-            {
+            var logout = function() {
                 localStorage.removeItem('userID');
                 localStorage.removeItem('token');
                 return true;
             }
-            var saveToken = function(token){
-                localStorage.setItem('token',token);
+            var saveToken = function(token) {
+                localStorage.setItem('token', token);
             }
             var getToken = function() {
                 return localStorage.getItem('token');
