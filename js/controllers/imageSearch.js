0cb54bd39e9898c8a2823886e985c341146b92fb
Adam Jones <adamjns802@gmail.com>
2016-08-02 12:50:00 -0400

    multiple image search sizes

--- js/controllers/imageSearch.js
+++ js/controllers/imageSearch.js
@@ -10,22 +10,22 @@
 
                 var search = vm.getInfo;
 
-                var data = API.getInfo(search,200);
+                var data = API.getInfo(search,125);
                 data.then(function(response) {
                     vm.data = response.data.value[0];
                 });
 
-                var data1 = API.getInfo(search,350);
+                var data1 = API.getInfo(search,250);
                 data1.then(function(response) {
                     vm.data1 = response.data.value[0];
                 });
 
-                var data2 = API.getInfo(search,500);
+                var data2 = API.getInfo(search,400);
                 data2.then(function(response) {
                     vm.data2 = response.data.value[0];
                 });
 
-                var data3 = API.getInfo(search,750);
+                var data3 = API.getInfo(search,600);
                 data3.then(function(response) {
                     vm.data3 = response.data.value[0];
                 });
