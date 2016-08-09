d6c47cf149a49d53dc8acae7fbbdb7c9d5c5c3b9
Adam Jones <adamjns802@gmail.com>
2016-08-09 16:58:06 -0400

    some saved image view work

---
+++ js/controllers/savedPhotos.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('flowers')
+        .controller('savedPhotosController', function(API, $state, back) {
+            var vm = this;
+
+
+            var projects = back.getSavedPhotos();
+            projects.then(function(response) {
+                console.log(response);
+                var projects = response.data.data;
+                vm.projects = projects;
+
+                vm.projects.forEach(function(projects){
+                    console.log("taco");
+                })
+
+
+            });
+        });
+})();
